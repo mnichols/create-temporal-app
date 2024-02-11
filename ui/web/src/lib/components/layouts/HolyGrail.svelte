1f86f97d37566af5eca74bc4ea94b15fc2e35244
Mike Nichols <mike.nichols@temporal.io>
2024-02-11 16:21:40 -0500

    update settings selector

--- ui/web/src/lib/components/layouts/HolyGrail.svelte
+++ ui/web/src/lib/components/layouts/HolyGrail.svelte
@@ -1,24 +1,24 @@
-<div class="grid grid-rows-6 grid-cols-12 gap-2 h-screen bg-gray-100">
-    <header class="row-auto col-span-12 bg-gray-300">
+<div class='grid grid-rows-6 grid-cols-12 gap-2 h-screen'>
+    <header class='row-auto col-span-12'>
         <slot name='header'/>
     </header>
 
-    <nav class="col-span-2 bg-gray-400 row-span-4">
+    <nav class='col-span-2 row-span-4'>
         <slot name='left-col'/>
         <!-- Navigation -->
     </nav>
 
-    <main class="col-span-7 bg-gray-500 row-span-4">
+    <main class='col-span-7 row-span-4'>
         <!-- Main content -->
         <slot name='main'/>
     </main>
 
-    <aside class="col-span-3 bg-gray-600 row-span-4">
+    <aside class='col-span-3 row-span-4'>
         <slot name='right-col'/>
         <!-- Sidebar / Ads -->
     </aside>
 
-    <footer class="row-auto col-span-12 bg-gray-300">
+    <footer class='row-auto col-span-12'>
         <slot name='footer'/>
         <!-- Footer content -->
     </footer>
