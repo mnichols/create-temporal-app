c842fad70ffe2c345b8a1a186569bcefd3bb6bb5
Mike Nichols <mike.nichols@temporal.io>
2024-03-27 17:58:04 +0000

    showing how to collect subscribed events

--- ui/web/src/routes/(public)/diag/+page.svelte
+++ ui/web/src/routes/(public)/diag/+page.svelte
@@ -21,8 +21,8 @@
         vals = [...vals, data.subPing.value]
     }
     // see here for subscriptionStore docs: https://commerce.nearform.com/open-source/urql/docs/api/svelte/
-    let ss = subscriptionStore({client, query: SubPingDocument, variables: {input: {value: 'foo'}}}, handleData)
-    ss.subscribe(arg2 => {
+    let messages = subscriptionStore({client, query: SubPingDocument, variables: {input: {value: 'foo'}}}, handleData)
+    messages.subscribe(arg2 => {
         if (arg2.error) {
             logger.error({
                 error: serializeError(arg2.error)
@@ -43,11 +43,24 @@
 
 </script>
 
-<h1>Subscription Diagnostics</h1>
+<header>
+    <h1>Simple Subscription Diagnostics</h1>
+    <h2>Default retry options for SSE Client found <a
+            about='_blank'
+            href='https://the-guild.dev/graphql/sse/docs/interfaces/client.ClientOptions#retry'>here</a>
+    </h2>
+</header>
+<div class='indicator'>
+    <span class="indicator-item indicator-start badge {$messages.error ? 'badge-error' : 'badge-accent'}"></span>
+    <h1 class="grid w-64 h-16 bg-base-300 place-items-center">Subscriptions Connected</h1>
+</div>
+
 <!--{#if $ss.fetching}-->
 <!--    <p>Loading...</p>-->
-{#if $ss.error}
-    <p>Error... {$ss.error.message}</p>
+{#if $messages.error}
+    <p class='text-error'>Error... {$messages.error.message}</p>
+{:else if showVals.length < 1}
+    <p class='text-secondary'>No Data</p>
 {:else}
     <ul>
         {#each showVals as sub, index}
@@ -55,4 +68,6 @@
         {/each}
     </ul>
 {/if}
+
+
 
--- ui/web/src/routes/(public)/diag/simple/+page.svelte
+++ ui/web/src/routes/(public)/diag/simple/+page.svelte
@@ -7,16 +7,26 @@
     let client = getContextClient()
     // see here for subscriptionStore docs: https://commerce.nearform.com/open-source/urql/docs/api/svelte/
     let messages = subscriptionStore({client, query: SubPingDocument, variables: {input: {value: 'foo'}}})
-
+    
 </script>
 
-<h1>Simple Subscription Diagnostics</h1>
+<header>
+    <h1>Simple Subscription Diagnostics</h1>
+    <h2>Default retry options for SSE Client found <a
+            about='_blank'
+            href='https://the-guild.dev/graphql/sse/docs/interfaces/client.ClientOptions#retry'>here</a>
+    </h2>
+</header>
+<div class='indicator'>
+    <span class="indicator-item indicator-start badge {$messages.error ? 'badge-error' : 'badge-accent'}"></span>
+    <h1 class="grid w-64 h-16 bg-base-300 place-items-center">Subscriptions Connected</h1>
+</div>
 <!--{#if $messages.fetching}-->
 <!--    <p>Loading...</p>-->
 {#if $messages.error}
-    <p>Error... {$messages.error.message}</p>
+    <p class='text-error'>Error... {$messages.error.message}</p>
 {:else if !$messages.data}
-    <p>No Data</p>
+    <p class='text-secondary'>No Data</p>
 {:else}
     <p>Current Value: {$messages.data.subPing.value}</p>
     <!--    <ul>-->
